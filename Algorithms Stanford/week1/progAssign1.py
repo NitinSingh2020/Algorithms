eaeeac42a46f34ec258eb3bbcfa773411b52c92a
Nitin Singh <nitin.singh2020@gmail.com>
2014-05-17 22:16:23 -0500

    Added Brute force count inversion algo

--- Algorithms Stanford/week1/progAssign1.py
+++ Algorithms Stanford/week1/progAssign1.py
@@ -2,6 +2,10 @@ import math
 PATH_TO_FILE = 'ProgAssign1.txt'
 
 def loadNumbers():
+	"""
+	reads txt file
+	returns: array/list of int in file
+	"""
 	lines = open(PATH_TO_FILE, 'r', 0).read().splitlines()
 	numberList = []
 	for a in lines:
@@ -9,13 +13,35 @@ def loadNumbers():
 	return numberList
 
 
-testList = [2,5,6,1,3,9,7]
+def merge(left, right):
+	"""
+	left: list of int
+	right: list of int
+	returns: sorted merge list 
+	"""
+	result = []
+	while len(left) > 0 or len(right) > 0:
+		if len(left) > 0 and len(right) > 0:
+			if left[0] <= right[0]:
+				result.append(left[0])
+				left = left[1:]
+			else:
+				result.append(right[0])
+				right = right[1:]
+		elif len(left) > 0:
+			result.append(left[0])
+			left = left[1:]
+		elif len(right) > 0:
+			result.append(right[0])
+			right = right[1:]
+	return result
+
 
 def mergeSort(inputList):
 	"""
 	inputList: list of integers to be sorted in increasing order
-	returns a new list sorted in increasing order
-	meregeSort is O[nlog(n)]
+	returns: a new list sorted in increasing order
+	Time Complexity: O[nlog(n)]
 	"""
 	if len(inputList) <= 1:
 		return inputList
@@ -35,24 +61,35 @@ def mergeSort(inputList):
 	return result
 
 
+def CountInv(inputList, n):
+	"""
+	inputList: list of integers
+	n: length/size of inputList
+	returns: number of invesions in inputList
 
-def merge(left, right):
-	result = []
-	while len(left) > 0 or len(right) > 0:
-		if len(left) > 0 and len(right) > 0:
-			if left[0] <= right[0]:
-				result.append(left[0])
-				left = left[1:]
-			else:
-				result.append(right[0])
-				right = right[1:]
-		elif len(left) > 0:
-			result.append(left[0])
-			left = left[1:]
-		elif len(right) > 0:
-			result.append(right[0])
-			right = right[1:]
-	return result
+	Brute Force Method with Time Complexity: O(n^2)
+	"""
+	count = 0
+	for i in range(n-1):
+		if (i%1000 == 0):
+			print("Done with step: ", i)
+		for j in range(i+1, n):
+			if (inputList[i] > inputList[j]):
+				count += 1
+	return count
 
-print(mergeSort(testList))
-print(mergeSort(loadNumbers()))
+
+testList1 = [2,5,6,1,3,9,7]
+testList2 = [1,3,5,2,4,6]
+
+
+
+
+if __name__ == "__main__":
+	print(mergeSort(testList1))
+	# print(mergeSort(loadNumbers()))
+	# print(SortAndMerge(testList1))
+	# print(SortAndMerge(testList2))
+	print(CountInv(testList1, 7))
+	print(CountInv(testList2, 6))
+	print(CountInv(loadNumbers(), 100000))
